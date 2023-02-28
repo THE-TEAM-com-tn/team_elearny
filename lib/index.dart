e0769daf72a3a8e320ab380a57e5efecef1b3064
iifast2 <60607576+iifast2@users.noreply.github.com>
2023-02-28 08:50:53 +0100

    Initial commit

---
+++ lib/index.dart
@@ -0,0 +1,11 @@
+// Export pages
+export 'sample_page/sample_page_widget.dart' show SamplePageWidget;
+export 'home_page/home_page_widget.dart' show HomePageWidget;
+export 'product_details_page/product_details_page_widget.dart'
+    show ProductDetailsPageWidget;
+export 'users_and_group_page/users_and_group_page_widget.dart'
+    show UsersAndGroupPageWidget;
+export 'training_details_page/training_details_page_widget.dart'
+    show TrainingDetailsPageWidget;
+export 'lesson_page/lesson_page_widget.dart' show LessonPageWidget;
+export 'settings_page/settings_page_widget.dart' show SettingsPageWidget;
